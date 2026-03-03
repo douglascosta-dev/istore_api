65b041630f5fc9a416f9b479c2c5794fbda6ef53
Douglas <douglascostateixeira777@gmail.com>
2026-03-03 20:33:07 -0300

    chore: fixed dto name in Role

--- src/modules/roles/dto/find-role-query.dto.ts
+++ src/modules/roles/dto/find-role-query.dto.ts
@@ -1,7 +1,7 @@
 import { IsOptional, IsString } from 'class-validator';
 import { PaginationQueryDTO } from 'src/common/dto/pagination.dto';
 
-export class FindRolesDTO extends PaginationQueryDTO {
+export class FindRolesQueryDTO extends PaginationQueryDTO {
   @IsString()
   @IsOptional()
   readonly name?: string;
