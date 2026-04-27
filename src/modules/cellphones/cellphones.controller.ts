970dfe02ca3b626b0af38d050a86f6b771909be6
Douglas <douglascostateixeira777@gmail.com>
2026-04-27 14:51:05 -0300

    feat:add guards for cellphone endpoints

--- src/modules/cellphones/cellphones.controller.ts
+++ src/modules/cellphones/cellphones.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { PaginatedResponse } from 'src/common/interfaces/paginated-response.interface';
 import { CellphoneResponse } from './dto/cellphone.response';
@@ -17,10 +18,15 @@ import { plainToInstance } from 'class-transformer';
 import { CreateCellphoneDTO } from './dto/create-cellphone.dto';
 import { UpdateCellphoneDTO } from './dto/update-cellphone.dto';
 import { CellphoneUserResponse } from './dto/cellphone-users.response';
+import { AuthGuard } from '@nestjs/passport';
+import { PermissionGuard } from 'src/common/guards/permission.guard';
+import { RequirePermissions } from 'src/common/decorators/role-permission.decorator';
 
 @Controller('cellphones')
+@UseGuards(AuthGuard('jwt'), PermissionGuard)
 export class CellphoneController {
   constructor(private readonly cellphoneService: CellphoneService) {}
+  @RequirePermissions('read_cellphone')
   @Get()
   async findAll(
     @Query() query: FindCellphoneQueryDTO,
@@ -33,7 +39,8 @@ export class CellphoneController {
       }),
     };
   }
-
+  @RequirePermissions('read_cellphone')
+  @RequirePermissions('read_user')
   @Get('/users')
   async findOneWithUsers(
     @Query() query: FindCellphoneQueryDTO,
@@ -41,6 +48,7 @@ export class CellphoneController {
     return await this.cellphoneService.findAllWithUsers(query);
   }
 
+  @RequirePermissions('read_cellphone')
   @Get(':id')
   async findOne(
     @Param('id', new ParseUUIDPipe()) id: string,
@@ -50,7 +58,7 @@ export class CellphoneController {
       excludeExtraneousValues: true,
     });
   }
-
+  @RequirePermissions('create_cellphone')
   @Post()
   async createdOne(
     @Body() body: CreateCellphoneDTO,
@@ -61,6 +69,7 @@ export class CellphoneController {
     });
   }
 
+  @RequirePermissions('update_cellphone')
   @Patch(':id')
   async updateOne(
     @Param('id', new ParseUUIDPipe()) id: string,
@@ -72,6 +81,7 @@ export class CellphoneController {
     });
   }
 
+  @RequirePermissions('delete_cellphone')
   @Delete(':id')
   async deleteOne(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.cellphoneService.deleteOne(id);
