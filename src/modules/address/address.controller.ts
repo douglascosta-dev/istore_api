b6eb5153f1dd1d02592703b9f175202d30d42f79
Douglas Costa <douglascostateixeira777@gmail.com>
2026-05-09 12:48:44 -0300

    feat:add guards for address endpoints

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressResponse } from './dto/address.response';
@@ -16,10 +17,14 @@ import { FindAddressQueryDTO } from './dto/find-address-query.dto';
 import { plainToInstance } from 'class-transformer';
 import { CreateAddressDTO } from './dto/create-adress.dto';
 import { UpdateAdressDTO } from './dto/update-adress.dto';
-
+import { AuthGuard } from '@nestjs/passport';
+import { PermissionGuard } from 'src/common/guards/permission.guard';
+import { RequirePermissions } from 'src/common/decorators/role-permission.decorator';
+@UseGuards(AuthGuard('jwt'), PermissionGuard)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
+  @RequirePermissions('read:address')
   @Get()
   async findAll(
     @Query() query: FindAddressQueryDTO,
@@ -33,6 +38,7 @@ export class AddressController {
     };
   }
 
+  @RequirePermissions('read:address')
   @Get(':id')
   async findOne(
     @Param('id', new ParseUUIDPipe()) id: string,
@@ -50,7 +56,7 @@ export class AddressController {
       excludeExtraneousValues: true,
     });
   }
-
+  @RequirePermissions('read:address', 'update:address', 'create:address')
   @Patch(':id')
   async updateOne(
     @Param('id', new ParseUUIDPipe()) id: string,
@@ -61,7 +67,7 @@ export class AddressController {
       excludeExtraneousValues: true,
     });
   }
-
+  @RequirePermissions('read:address', 'delete:address', 'update:address')
   @Delete(':id')
   async deleteOne(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.addressService.deleteOne(id);
--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -4,9 +4,10 @@ import { AddressService } from './address.service';
 import { Address } from './entities/address.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { RolePermissionModule } from '../role_permissions/role-permission.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Address, User])],
+  imports: [TypeOrmModule.forFeature([Address, User]), RolePermissionModule],
   controllers: [AddressController],
   providers: [AddressService],
   exports: [],
